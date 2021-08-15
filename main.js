4e2bfa188874614be30c17ba92057f637cae4da3
AJ-Brown-InTech <ajalantbrown@gmail.com>
2021-08-14 20:22:50 -0700

    add a section to our about section with moving text

---
+++ main.js
@@ -0,0 +1,8 @@
+//Create a clone for our section in the about section
+const ticker = document.querySelector('.ticker')
+, ticker_item = document.querySelector('.ticker-item')
+, clone = ticker_item.cloneNode(true)
+
+ticker.append(clone++)
+
+
