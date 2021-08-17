410f7759dcde380e2d95a18c1d025a8345403f9c
AJ-Brown-InTech <ajalantbrown@gmail.com>
2021-08-16 18:18:29 -0700

    the ticker in the about section is okay for now maybe not 100% of what i want it to be. i may comback later

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 //Create a clone for our section in the about section
-const ticker = document.querySelector('.ticker')
-, ticker_item = document.querySelector('.ticker-item')
-, clone = ticker_item.cloneNode(true)
+var ticker = document.querySelector('.ticker'), 
+ticker_item = document.querySelector('.ticker-item'),
+clone = ticker_item.cloneNode(true)
+//this is for the ticker to have a clone of the same text but a bit smaller
+ ticker_item.append(clone)
+console.log(clone)
 
-ticker.append(clone++)
+
+
 
 
