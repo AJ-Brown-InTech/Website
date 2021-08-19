56a16f2132adf72bcb309e23403679564719b8f9
AJ-Brown-InTech <ajalantbrown@gmail.com>
2021-08-19 10:27:02 -0700

    cards are going pretty good need to make them a bit more dynamic more styling + animations

--- public/main.js
+++ public/main.js
@@ -19,8 +19,7 @@ var cardData = [
     {title: "HTML5/CSS3", description: " I USE THESE TECHNOLOGIES TO BRING INNOVATION & STRUCTURE TO BROWSERS, SOME ANIMATIONS TOO"},
     {title: "JAVASCRIPT", description: " I USE THIS TECHNOLOGY TO CREATE ANIMATIONS AND EFFECTS BUT ALSO HANDLE ALL THAT DATA"},
     {title: "REACT.JS", description: " I USE THIS TO CREATE DYNAMIC APPLICATIONS"},
-    {title: "NODE.JS", description: " I USE IT FOR WRTING SERVERS AND HANDLING REQUEST"},
-    {title: "EXPRESS", description: " I USE THIS FRAMEWORK FOR WORKING WITH THE BACKEND IN NODE.JS"},
+    {title: "NODE/EXPRESS", description: " I USE THIS FRAMEWORK AND RUNTIME FOR WORKING WITH SERVERS AND HANDLING REQUEST"},
     {title: "GIT", description: " I USE THIS VERSION CONTROL FOR SOURCE CODE MANAGEMENT, GREAT PART IS IT'S BASH IS ALMOST IDENTICAL TO LINUX'S TERMINAL"},
     {title: "VISUAL STUDIO CODE", description: " THIS IS MY PREFERED INTEGRATED DEVELOPMENT ENVIROMENT"},
     {title: "LINUX/ WINDOWS 10", description: " THESE ARE THE TWO OPERATING SYTEMS I USE, I'VE SETUP THESE ENVIROMENTS ON MY COMPUTERS"},
@@ -28,14 +27,11 @@ var cardData = [
     {title: "HEROKU", description: " I USE THIS TO DEPLOY AND MANAGE WEB APPLICATIONS TECHNOLOGIES TO BRING INNOVATION & STRUCTURE TO BROWSERS"},
 ]
 //Card data function that builds the html for our cards
-function getHTMLForCards(){
-    return ` <div class="card" style="width: 18rem;">
+function getHTMLForCards(cardData){
+    return ` <div class="card" >
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+      <h5 class="card-title">${cardData.title}</h5>
+      <p class="card-text">${cardData.description}.</p>
     </div>
   </div> `
 }
