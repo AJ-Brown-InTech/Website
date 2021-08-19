e0b4d1efdf6647034ced20a7e5acedeb6d2dfcd6
AJ-Brown-InTech <ajalantbrown@gmail.com>
2021-08-19 09:41:15 -0700

    server works for the site communicating with node back to builindg the front end

--- node.js
+++ node.js
@@ -1,19 +1,18 @@
+//imports
 "use strict"
-const http = require('http')
 const express = require('express')
 const port = 5000
-
 const app = express()
+
 //middleware to retreive my static files
 app.use(express.static('public'))
-app.use('/images', express.static(__dirname + 'public/images'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/css', express.static(__dirname + 'public/style.css'))
+app.use('/js', express.static(__dirname + 'public/main.js'))
 
 //specifically for retreiving my html
 app.get('', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html' )
+    res.sendFile(__dirname + '/public/index.html')
 })
 
-
+//listening on port 5000
 app.listen(port, () => console.info(`Listening on port ${port}. `))
