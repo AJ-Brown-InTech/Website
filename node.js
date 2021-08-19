edf70c90383d080681199e93996cd404cd6f8100
AJ-Brown-InTech <ajalantbrown@gmail.com>
2021-08-19 08:03:36 -0700

    finished how i want this project to be structered

--- node.js
+++ node.js
@@ -4,7 +4,15 @@ const express = require('express')
 const port = 5000
 
 const app = express()
+//middleware to retreive my
+app.use(express.static('public'))
+app.use('/Images', express.static(__dirname + 'public/Images'))
+app.use('/css', express.static(__dirname + 'public/style.css'))
+app.use('/js', express.static(__dirname + 'public/main.js'))
 
-app.listen(port, () =>{
-    console.info(`Listening on port ${port}. `)
+app.get('/', (req, res) => {
+    res.sendFile('index')
 })
+
+
+app.listen(port, () => console.info(`Listening on port ${port}. `))
